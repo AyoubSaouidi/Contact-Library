0e072919f8657a1bed84cae9fc3fcac93e944955
Ayoub Saouidi <saouidi.ayoub.99@gmail.com>
2021-10-24 09:47:40 +0100

    Heroku Deployement Setup

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 const connectDB = require('./config/db');
 
 // Create Express App
@@ -14,6 +15,17 @@ app.use(express.json({ extended: false }));
 app.use('/api/users', require('./routes/users'));
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/contacts', require('./routes/contacts'));
+
+// Load static assets in production
+if (process.env.NODE_ENV === 'production') {
+    // Set static folder
+    app.use(express.static('client/build'));
+
+    // Send index.html for every Route...
+    app.get('*', (req, res) =>
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    );
+}
 
 const PORT = process.env.PORT || 5000;
 // Run application
